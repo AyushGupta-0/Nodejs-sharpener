41f74d50a21dde39ce61df6e27c727e11bff9cc8
Ayush <ayush5555.ag@gmail.com>
2023-09-29 19:55:41 +0530

    Routing

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
+
 const http = require("http");
 const express = require("express");
-const bosyParser = require("body-parser")
+const bosyParser = require("body-parser");
+
 
 const app = express();
 
-app.use(bosyParser.urlencoded({extended:false}));
+const adminRoutes = require("./routes/admin");
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title" type="submit"><button>Add Product</button></form>'
-  );
-});
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from Express!</h1>");
-});
+const shopRoutes = require("./routes/shop");
+
+app.use(bosyParser.urlencoded({ extended: false }));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use((req , res,next)=>{
+  res.status(404).send('<h1>Page not found</h1>');
+})
 
 const server = http.createServer(app);
 
